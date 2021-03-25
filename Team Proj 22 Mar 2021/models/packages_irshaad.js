dfb9e233a14c137021d30489b3be82f8295ec7d3
irshaad-sait <Irshaad.Sardiwalla@edu.sait.ca>
2021-03-25 14:59:31 -0600

    commit 25 Mar

---
+++ Team Proj 22 Mar 2021/models/packages_irshaad.js
@@ -0,0 +1,31 @@
+// getting-started.js
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+
+const packageSchema = new Schema({
+  PackageId: Number,
+  PkgName: String,
+  PkgStartDate: Date,
+  PkgEndDate: Date,
+  PkgDesc: String,
+  PkgBasePrice: Number,
+  PkgAgencyCommission: Number,
+  PkgImg: String,
+});
+
+module.exports = mongoose.model("Package", packageSchema);
+
+
+
+// (err, client) => {
+//     const db = client.db("myclouddb");     
+//     const mypackages = db.collection("packages");
+//     mypackages.find().toArray((err, dbpackages) => {
+//        return dbpackages;
+//       });
+//   }
+
+
+
+
